d4d54c9ed40083609f9de5fcb88f9e187188a1fa
Tony Mhlanga <34751292+vmhlanga@users.noreply.github.com>
2025-12-11 18:28:58 -0600

    Add server catalog admin UI and target .NET 10

--- src/DataPulse.Web/Controllers/AdminController.cs
+++ src/DataPulse.Web/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Threading.Tasks;
 using DataPulse.Application.Services;
+using DataPulse.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +23,78 @@ namespace DataPulse.Web.Controllers
         {
             var processes = await _processCatalogService.GetProcessesAsync();
             return View(processes);
+        }
+
+        [HttpGet("servers")]
+        public async Task<IActionResult> Servers()
+        {
+            var viewModel = await BuildCatalogViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost("servers/add")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddServer(NewAgentInput newAgent)
+        {
+            if (!ModelState.IsValid)
+            {
+                var invalidModel = await BuildCatalogViewModel();
+                invalidModel.NewAgent = newAgent;
+                return View("Servers", invalidModel);
+            }
+
+            await _processCatalogService.AddAgentAsync(newAgent.AgentName);
+            return RedirectToAction(nameof(Servers));
+        }
+
+        [HttpPost("servers/jobs/add")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddJob(NewJobInput newJob)
+        {
+            if (!ModelState.IsValid)
+            {
+                var invalidModel = await BuildCatalogViewModel();
+                invalidModel.NewJob = newJob;
+                return View("Servers", invalidModel);
+            }
+
+            var request = new ProcessCatalogCreateRequest
+            {
+                ProcessName = newJob.ProcessName,
+                ProcessDescription = newJob.ProcessDescription,
+                ProcessTypeId = newJob.ProcessTypeId,
+                AgentMasterId = newJob.AgentMasterId,
+                PrimaryStep = new StepCatalogCreateRequest
+                {
+                    StepName = newJob.StepName,
+                    StepDescription = newJob.StepDescription,
+                    ServerName = newJob.ServerName,
+                    DatabaseName = newJob.DatabaseName,
+                    SpName = newJob.SpName,
+                    SsisPackageName = newJob.SsisPackageName,
+                    SsisSolutionName = newJob.SsisSolutionName,
+                    ExecutePath = newJob.ExecutePath
+                }
+            };
+
+            await _processCatalogService.AddProcessAsync(request);
+            return RedirectToAction(nameof(Servers));
+        }
+
+        private async Task<CatalogAdminViewModel> BuildCatalogViewModel()
+        {
+            var agents = await _processCatalogService.GetAgentsAsync();
+            var processes = await _processCatalogService.GetProcessesAsync();
+
+            return new CatalogAdminViewModel
+            {
+                Agents = agents,
+                Processes = processes,
+                NewJob = new NewJobInput
+                {
+                    AgentMasterId = agents.FirstOrDefault()?.AgentMasterId ?? 0
+                }
+            };
         }
     }
 }
